a3b566770360b8364f5a74c363dd6bfc89cc39ef
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2022-06-26 21:10:41 +0200

    remove flex-layout

--- projects/editor/src/app/home/home.module.ts
+++ projects/editor/src/app/home/home.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -12,7 +11,6 @@ import { MatButtonModule } from '@angular/material/button';
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    FlexLayoutModule,
     MatToolbarModule,
     MatButtonModule,
     RouterModule.forChild([{ path: '', component: HomeComponent }])
